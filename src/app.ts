f032e0f9deb2d09cbe40575fbd587d0f251c6b8c
Derek Oliveira <derek.foliveira@gmail.com>
2026-08-22 18:06:23 -0300

    Add: middlewares and AppError

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express  from "express";
+import "express-async-error"
+import { errorHandling } from "@/middlewares/error-handling";
 
 const app = express()
 
 app.use(express.json())
+app.use(errorHandling)
 
 export { app }
